033d874b66b8efc6ac3c64f96ca6c7a2d4af1764
Joe Kim <josephkim0224@gmail.com>
2017-06-14 14:56:39 -0400

    some additional code improvements and added more error messages and data validations

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -12,13 +12,19 @@ class AccountsController < ApplicationController
     end
 
     def process_ids(id)
-      @account = current_user.accounts.find_by(id: id)
-      redirect "/accounts" if !@account
+      @account = users_account_from_id(id)
+      if !@account 
+        flash[:message] = "You either do not have access to this account, or this account simply does not exist"
+        redirect "/accounts" if !@account
+      end
     end
 
     def valid_params?(params)
       if params[:name].empty? || params[:balance].empty?
         flash[:message] = "VALIDATION ERROR: Please ensure that both name and balance fields are filled out"
+        false
+      elsif params[:balance].to_i < 0
+        flash[:message] = "VALIDATION ERROR: Balance must be greater than or equal to 0"
         false
       else
         true
@@ -32,6 +38,7 @@ class AccountsController < ApplicationController
 
   get '/accounts' do
     redirect_if_not_logged_in
+
     @accounts = current_user.accounts
     erb :"accounts/index"
   end
@@ -44,13 +51,14 @@ class AccountsController < ApplicationController
 
   get '/accounts/:id' do
     redirect_if_not_logged_in
-    process_ids(params[:id])
 
+    process_ids(params[:id])
     erb :"accounts/show"
   end
 
   get '/accounts/:id/edit' do
     redirect_if_not_logged_in
+
     process_ids(params[:id])
     @account = users_account_from_id(params[:id])
     erb :"accounts/edit"
@@ -60,10 +68,7 @@ class AccountsController < ApplicationController
     redirect_if_not_logged_in
     redirect "/accounts/new" if !valid_params?(params)
 
-    account = Account.new
-    account.name = params[:name]
-    account.balance = params[:balance]
-    account.user = current_user
+    account = current_user.accounts.build(params)
     account.save
 
     redirect "/accounts"
@@ -72,6 +77,7 @@ class AccountsController < ApplicationController
   patch '/accounts/:id' do
     redirect_if_not_logged_in
     redirect "/accounts/#{params[:id]}/edit" if !valid_params?(params)
+
     account = users_account_from_id(params[:id])
     account.name = params[:name]
     account.balance = params[:balance]
