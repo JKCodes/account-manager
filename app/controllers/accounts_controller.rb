dae4eea24bf209094efeb765b594f16f808fe10c
Joe Kim <josephkim0224@gmail.com>
2017-06-14 09:57:31 -0400

    every action in accounts requires login

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -1,27 +1,41 @@
 class AccountsController < ApplicationController
 
   get '/accounts' do
+    redirect_if_not_logged_in
+
     erb :"accounts/index"
   end
 
   get '/accounts/new' do
+    redirect_if_not_logged_in
+
     erb :"accounts/new"
   end
 
   get '/accounts/:id' do
+    redirect_if_not_logged_in
+
     erb :"accounts/show"
   end
 
   get '/accounts/:id/edit' do
+    redirect_if_not_logged_in
+
     erb :"accounts/edit"
   end
 
   post '/accounts' do
+    redirect_if_not_logged_in
+
   end
 
   patch '/accounts/:id' do
+    redirect_if_not_logged_in
+
   end
 
   delete '/accounts/:id/delete' do
+    redirect_if_not_logged_in
+    
   end
 end
